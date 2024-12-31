2c9ce484775c9642cbc48c1dd608585a30cea12d
masa1234 <17587674+masa1234@users.noreply.github.com>
2024-12-31 10:35:07 +0900

    ArticleDTO

--- src/main/java/com/example/blog/web/controller/article/ArticleRestController.java
+++ src/main/java/com/example/blog/web/controller/article/ArticleRestController.java
@@ -1,5 +1,6 @@
 package com.example.blog.web.controller.article;
 
+import java.time.LocalDateTime;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +10,12 @@ public class ArticleRestController {
 
   //Get /articles/a
   @GetMapping("/articles/{id}")
-  public String showArticle(@PathVariable("id") long id){
-    return "This is artice: id = " + id;
+  public ArticleDTO showArticle(@PathVariable("id") long id){
+    return new ArticleDTO(id,
+        "this is title : "+id,
+        "this is content",
+        LocalDateTime.now(),
+        LocalDateTime.now());
   }
 
 }
