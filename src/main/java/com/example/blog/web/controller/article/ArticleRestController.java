d6379a85266f632d8df3a50afb05d2fafedd5c79
masa1234 <17587674+masa1234@users.noreply.github.com>
2025-01-02 15:24:05 +0900

    lombok

--- src/main/java/com/example/blog/web/controller/article/ArticleRestController.java
+++ src/main/java/com/example/blog/web/controller/article/ArticleRestController.java
@@ -2,6 +2,7 @@ package com.example.blog.web.controller.article;
 
 import com.example.blog.service.article.ArticleService;
 import java.time.LocalDateTime;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,13 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
+@RequiredArgsConstructor
 public class ArticleRestController {
 
   private final ArticleService articleService;
 
-  public ArticleRestController(ArticleService articleService){
-    this.articleService = articleService;
-  }
   //Get /articles/a
   @GetMapping("/articles/{id}")
   public ArticleDTO showArticle(@PathVariable("id") long id){
