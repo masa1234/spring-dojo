b0a0c3b2eb245c6191dd9c25d86d3b98e74d6976
masa1234 <17587674+masa1234@users.noreply.github.com>
2025-01-02 14:44:33 +0900

    Article.DTO

--- src/main/java/com/example/blog/web/controller/article/ArticleDTO.java
+++ src/main/java/com/example/blog/web/controller/article/ArticleDTO.java
@@ -1,5 +1,6 @@
 package com.example.blog.web.controller.article;
 
+import com.example.blog.service.article.ArticleEntity;
 import java.time.LocalDateTime;
 
 public record ArticleDTO (
@@ -9,4 +10,13 @@ public record ArticleDTO (
   LocalDateTime createdAt,
   LocalDateTime updatedAt
       ){
+      public static ArticleDTO from(ArticleEntity entity){
+            return new ArticleDTO(
+                entity.id(),
+                entity.title(),
+                entity.content(),
+                entity.createdAt(),
+                entity.updatedAt()
+            );
+      }
 }
